b9127d67fac2aac192cafc58fce0bcf6b5318130
Jack Machin <jack.machin125@gmail.com>
2017-01-31 11:30:07 +0000

    Main

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
                 <?php wp_nav_menu(array(
                     'container' => 'div',                           // remove nav container
                     'container_class' => 'container',                 // class of container (should you choose to use it)
-                    'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+                    'menu' => __( 'Main', 'bonestheme' ),  // nav name
                     'menu_class' => 'nav top-nav',               // adding custom nav class
                     'theme_location' => 'main-nav',                 // where it's located in the theme
                     'before' => '',                                 // before the menu
