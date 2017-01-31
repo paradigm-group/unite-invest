80f2bc4675f2f84b1d4bfdb44d6bbfa168048ec7
Jack Machin <jack.machin125@gmail.com>
2017-01-31 14:09:24 +0000

    Invest

--- page.php
+++ page.php
@@ -20,25 +20,25 @@
                             <h3>Starter</h3>
                             <div class="price">
                                 <div class="price-figure">
-                                    <span class="price-number">FREE</span>
+                                    <span class="price-number">Software and Support</span>
                                 </div>
                             </div>
                             <ul class="features">
-                                <li>1GB Storage</li>
-                                <li>2 Clients</li>
-                                <li>5 Active Projects</li>
-                                <li>5 Colors</li>
-                                <li>Free Goodies</li>
-                                <li>24/7 Email support</li>
+                                <li>Feature 1</li>
+                                <li>Feature 2</li>
+                                <li>Feature 3</li>
+                                <li>Feature 4</li>
+                                <li>Feature 5</li>
+                                <li>Feature 6</li>
                             </ul>
                             <div class="footer">
-                                <a href="#" class="btn btn-inverse btn-block">Buy Now</a>
+                                <a href="#" class="btn btn-inverse btn-block">Invest</a>
                             </div>
                         </div>
                     </li>
                     <li data-animation="true" data-animation-type="fadeInUp" class="fadeInUp contentAnimated finishAnimated">
                         <div class="pricing-container">
-                            <h3>Basic</h3>
+                            <h3>GIA</h3>
                             <div class="price">
                                 <div class="price-figure">
                                     <span class="price-number">$9.99</span>
@@ -46,21 +46,21 @@
                                 </div>
                             </div>
                             <ul class="features">
-                                <li>2GB Storage</li>
-                                <li>5 Clients</li>
-                                <li>10 Active Projects</li>
-                                <li>10 Colors</li>
-                                <li>Free Goodies</li>
-                                <li>24/7 Email support</li>
+                                <li>Feature 1</li>
+                                <li>Feature 2</li>
+                                <li>Feature 3</li>
+                                <li>Feature 4</li>
+                                <li>Feature 5</li>
+                                <li>Feature 6</li>
                             </ul>
                             <div class="footer">
-                                <a href="#" class="btn btn-inverse btn-block">Buy Now</a>
+                                <a href="#" class="btn btn-inverse btn-block">Invest</a>
                             </div>
                         </div>
                     </li>
                     <li class="highlight fadeInUp contentAnimated finishAnimated" data-animation="true" data-animation-type="fadeInUp">
                         <div class="pricing-container">
-                            <h3>Premium</h3>
+                            <h3>ISA</h3>
                             <div class="price">
                                 <div class="price-figure">
                                     <span class="price-number">$19.99</span>
@@ -68,12 +68,12 @@
                                 </div>
                             </div>
                             <ul class="features">
-                                <li>5GB Storage</li>
-                                <li>10 Clients</li>
-                                <li>20 Active Projects</li>
-                                <li>20 Colors</li>
-                                <li>Free Goodies</li>
-                                <li>24/7 Email support</li>
+                                <li>Feature 1</li>
+                                <li>Feature 2</li>
+                                <li>Feature 3</li>
+                                <li>Feature 4</li>
+                                <li>Feature 5</li>
+                                <li>Feature 6</li>
                             </ul>
                             <div class="footer">
                                 <a href="#" class="btn btn-theme btn-block">Buy Now</a>
@@ -82,22 +82,22 @@
                     </li>
                     <li data-animation="true" data-animation-type="fadeInUp" class="fadeInUp contentAnimated finishAnimated">
                         <div class="pricing-container">
-                            <h3>Lifetime</h3>
+                            <h3>SIPP</h3>
                             <div class="price">
                                 <div class="price-figure">
                                     <span class="price-number">$999</span>
                                 </div>
                             </div>
                             <ul class="features">
-                                <li>Unlimited Storage</li>
-                                <li>Unlimited Clients</li>
-                                <li>Unlimited Projects</li>
-                                <li>Unlimited Colors</li>
-                                <li>Free Goodies</li>
-                                <li>24/7 Email support</li>
+                                <li>Feature 1</li>
+                                <li>Feature 2</li>
+                                <li>Feature 3</li>
+                                <li>Feature 4</li>
+                                <li>Feature 5</li>
+                                <li>Feature 6</li>
                             </ul>
                             <div class="footer">
-                                <a href="#" class="btn btn-inverse btn-block">Buy Now</a>
+                                <a href="#" class="btn btn-inverse btn-block">Invest</a>
                             </div>
                         </div>
                     </li>
