5588c79f0ffed5aff0711f3d1e67b99e8ad698cd
Jack Machin <jack.machin125@gmail.com>
2017-02-10 10:44:58 +0000

    Try this again

--- index.php
+++ index.php
@@ -847,7 +847,7 @@
         </div>
         <!-- end #footer -->
         
-        <!-- begin theme-panel>
+        <!-- begin theme-panel-->
         <div class="theme-panel">
             <a href="javascript:;" data-click="theme-panel-expand" class="theme-collapse-btn"><i class="fa fa-cog"></i></a>
             <div class="theme-panel-content">
